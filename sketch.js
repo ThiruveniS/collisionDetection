fb2b0bc89c42bd5c7cbb0c5ebbdda7f9f4f5d2a8
ThiruveniS <53971412+ThiruveniS@users.noreply.github.com>
2020-12-10 21:01:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,14 +9,17 @@ function setup() {
   o2=createSprite(150,200,60,20);
   o3=createSprite(250,200,60,20);
   o4=createSprite(350,200,60,20);
+
+  movingRect.velocityX = 5;
+  fixedRect.velocityX = -5;
 }
 
 function draw() {
   background(255,255,255); 
-  movingRect.x=World.mouseX
-  movingRect.y=World.mouseY
+  //movingRect.x=World.mouseX
+  //movingRect.y=World.mouseY
    
-  if(istouching(movingRect,o3)){
+ /* if(istouching(movingRect,o3)){
     movingRect.shapeColor="red";
     o3.shapeColor="red";
 
@@ -24,8 +27,11 @@ function draw() {
   else{
     movingRect.shapeColor="green";
     o3.shapeColor="grey";
+   } */
 
-  }
+    bounceOff(movingRect,fixedRect);
+
+  
 
   drawSprites();
 }
@@ -46,4 +52,17 @@ else {
 
   return false;
 }
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+    object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1);
+  }
+  if (object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY * (-1);
+  } 
 }
